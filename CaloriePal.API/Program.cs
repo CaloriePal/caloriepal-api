0e4dfcb879ae5e647cd020b22f4f053d5827fc84
Cristian M <140653540+tonymocanu97@users.noreply.github.com>
2026-04-22 01:52:04 +0300

    Update Program.cs

--- CaloriePal.API/Program.cs
+++ CaloriePal.API/Program.cs
@@ -65,7 +65,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:3000")
+            .WithOrigins("https://caloriepal-web.vercel.app", "http://localhost:3000")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
